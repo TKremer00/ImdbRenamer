2df8ec8ac132f1eb0ac65f5b69f48a25717f196c
TKremer00 <tim.kremer1@student.graafschapcollege.nl>
2020-04-13 14:04:35 +0200

    fixed bug

--- src/file/FileSorter.java
+++ src/file/FileSorter.java
@@ -20,13 +20,20 @@ class FileSorter {
     // get episode number
     private static int getEpisodeNumber(String template, String toFormat)
     {
+        String search = "%d";
         String templateNoSpace = template.replaceAll(" ","");
         String toFormatNospace = toFormat.replaceAll(" ","");
-        int index = templateNoSpace.lastIndexOf("%d");
-        char after = templateNoSpace.charAt(index  + "%d".length());
-
-        String removeFirst = toFormatNospace.substring(index);
-        String removeLast = removeFirst.substring(0, removeFirst.lastIndexOf(after));
-        return Integer.parseInt(removeLast);
+        int index = templateNoSpace.lastIndexOf(search);
+        char after;
+        if(index + search.length() < templateNoSpace.length()) {
+            after = templateNoSpace.charAt(index + search.length());
+            String removeFirst = toFormatNospace.substring(index);
+            String removeLast = removeFirst.substring(0, removeFirst.lastIndexOf(after));
+            return Integer.parseInt(removeLast);
+        }else {
+            String removeFirst = toFormatNospace.substring(index);
+            String removeExtention = removeFirst.substring(0, removeFirst.lastIndexOf("."));
+            return Integer.parseInt(removeExtention);
+        }
     }
 }
